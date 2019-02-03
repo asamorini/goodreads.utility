f3c4c9944543080a2ec409345e74947b7fe62be7
Andrea Samorini <samorini@gmail.com>
2019-02-03 01:46:31 +0100

    Create config.samo.js

---
+++ src/js/config.samo.js
@@ -0,0 +1,26 @@
+/*
+https://www.w3schools.com/charsets/ref_utf_symbols.asp
+&#9888;	attenzione		check
+&#9989;	spunta verde	readNext
+&#9992;	aeroplano		w_
+&#10004;	spunta nera		readNext
+&#10067;	punto interrogativo 	check 
+&#10102;	numero 1		biblio
+&#10103;	numero 2
+&#10104;	numero 3
+&#10105;	numero 4
+&#10106;	numero 5
+&#10107;	numero 6
+&#10108;	numero 7
+&#9873;	bandiera nera	w_
+&#9787;		smile Nero		authors
+&Eta;		H				history
+&Tau;		T				tema
+&#Wopf;	W				w_
+&#9864;	cerchio nero buco bianco		genre
+&#9830;	seme carte quadri			genre
+&#9829;	cuore			readNext
+&#9856;	quadrarto con punto centrale		tema
+&#9839;	diesis		tema
+*/
+var samoGoodreadsUtility={lang:'ita',shelvesNames:'toread-,1-read-next|&#10004; Read next,2-|&#10103; ,3-|&#10104; ,g_|GENERE ,storico-|STORICO ,t_|&#9839; ,w_|&#9873; ,zz_|&#9787; ',excludeShelves:{read:{s:'p_',c:'',e:''},'to-read':{s:'',c:'',e:''}}};
