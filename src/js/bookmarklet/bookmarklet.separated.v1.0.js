8b4b6b96694b813822dc29a2d6500dc123027e34
Andrea Samorini <samorini@gmail.com>
2019-02-05 20:58:12 +0100

    Update bookmarklet.separated.v1.0.js

--- src/js/bookmarklet/bookmarklet.separated.v1.0.js
+++ src/js/bookmarklet/bookmarklet.separated.v1.0.js
@@ -1,1 +1,1 @@
-javascript:jQuery.ajaxSetup({cache:true});jQuery.getScript('https://asamorini.github.io/goodreads.utility/src/js/config.samo.js',function(){jQuery.getScript('https://asamorini.github.io/goodreads.utility/dist/goodreads.utility.min.js');});
+javascript:jQuery.ajaxSetup({cache:true});jQuery.getScript('https://asamorini.github.io/goodreads.utility/src/js/bookmarklet/config.samo.js',function(){jQuery.getScript('https://asamorini.github.io/goodreads.utility/dist/goodreads.utility.min.js');});
