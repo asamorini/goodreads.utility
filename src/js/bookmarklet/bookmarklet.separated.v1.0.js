ade72e3974d66df76817d2d60e72ff2613fe814e
Andrea Samorini <samorini@gmail.com>
2019-02-05 20:57:02 +0100

    Create bookmarklet.separated.v1.0.js

---
+++ src/js/bookmarklet/bookmarklet.separated.v1.0.js
@@ -0,0 +1,1 @@
+javascript:jQuery.ajaxSetup({cache:true});jQuery.getScript('https://asamorini.github.io/goodreads.utility/src/js/config.samo.js',function(){jQuery.getScript('https://asamorini.github.io/goodreads.utility/dist/goodreads.utility.min.js');});
