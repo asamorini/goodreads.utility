7c089b787c7d4faac575d4fa502e7cb1b6e2b524
Andrea Samorini <samorini@gmail.com>
2019-02-05 21:13:52 +0100

    Update config.samo.js

--- src/js/bookmarklet/config.samo.js
+++ src/js/bookmarklet/config.samo.js
@@ -25,19 +25,22 @@ https://www.w3schools.com/charsets/ref_utf_symbols.asp
 */
 var samoGoodreadsUtility={
   lang:'ita',
-  shelvesNames:['toread-',
-                '1-read-next|&#10004;Read next',
-                '2-|&#10103; ',
-                '3-|&#10104; ',
-                'aaa-to-check|&#10067;To check',
-                'g_|GENERE ',
-                'storico-|STORICO ',
-                't_|&#9839;',
-                'w_|&#9873;',
-                'zz_|&#9787; '
-  ].join(','),
-  excludeShelves:{
-    read:{s:'p_',c:'',e:''},
-    'to-read':{s:'',c:'',e:''}
+  shelvesViewer:{
+    analyzeShelfDefault:'to-read',
+    shelvesNames:['toread-',
+                  '1-read-next|&#10004;Read next',
+                  '2-|&#10103; ',
+                  '3-|&#10104; ',
+                  'aaa-to-check|&#10067;To check',
+                  'g_|GENERE ',
+                  'storico-|STORICO ',
+                  't_|&#9839;',
+                  'w_|&#9873;',
+                  'zz_|&#9787; '
+    ].join(','),
+    excludeShelves:{
+      read:{s:'p_',c:'',e:''},
+      'to-read':{s:'',c:'',e:''}
+    }
   }
 };
